c871e83b0fed98624e63a8d995e4d2a3978ebd13
TheEvilJade <115946998+TheEvilJade@users.noreply.github.com>
2023-03-28 12:28:48 +0200

    Update clickable_header.js

--- clickable_header.js
+++ clickable_header.js
@@ -2,4 +2,5 @@ const header = document.querySelector(`header`)
 
 header.addEventListener(`click`, function() {
     window.location.href = `https://theeviljade.github.io/`
+    header.style.cursor = `pointer`;
 })
